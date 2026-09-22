c9dc62f117d57305bd49faebce3b8a38bd2929f0
Milos Pruginic <milos_pruginic@yahoo.com>
2026-09-21 23:08:46 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = {};
+
+        for(const str of strs) {
+            const count = new Array(26).fill(0);
+
+            for(const char of str) {
+                count[char.charCodeAt(0) - 97]++;
+            }
+
+            const key = count.join(',');
+
+            if(!map[key]) {
+                map[key] = [];
+            }
+            map[key].push(str);
+        }
+
+        return Object.values(map);
+    }
+}
