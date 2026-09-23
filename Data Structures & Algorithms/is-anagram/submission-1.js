a06310800d879eb9509c228700c52e1d95ed528d
Milos Pruginic <milos_pruginic@yahoo.com>
2026-09-22 21:34:13 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length) return false;
+
+        let sHash = {};
+        let tHash = {};
+
+        for(let i = 0; i < s.length; i++) {
+            if(sHash[s[i]]) {
+                    sHash[s[i]]++;
+                } else {
+                    sHash[s[i]] = 1;
+                }
+        }
+
+        for(let i = 0; i < t.length; i++) {
+            if(tHash[t[i]]) {
+                    tHash[t[i]]++;
+                } else {
+                    tHash[t[i]] = 1;
+                }
+        }
+
+        const keys1 = Object.keys(sHash);
+
+        for (let key of keys1) {
+            if (sHash[key] !== tHash[key]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
