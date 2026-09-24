b76f4cc41338c85ed3442d10feea729fcd4f2406
Milos Pruginic <milos_pruginic@yahoo.com>
2026-09-23 22:48:09 -0400

    Add: design-hashmap - submission-2

---
+++ Data Structures & Algorithms/design-hashmap/submission-2.js
@@ -0,0 +1,41 @@
+class MyHashMap {
+    constructor() {
+        this.map = {};
+    }
+
+    /**
+     * @param {number} key
+     * @param {number} value
+     * @return {void}
+     */
+    put(key, value) {
+        this.map[key] = value;
+    }
+
+    /**
+     * @param {number} key
+     * @return {number}
+     */
+    get(key) {
+        if (this.map[key] !== undefined) {
+            return this.map[key];
+        }
+        return -1;
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    remove(key) {
+        delete this.map[key]; 
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
