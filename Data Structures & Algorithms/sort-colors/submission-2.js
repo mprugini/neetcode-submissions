08249e49d085aefb9913126f0d2425b2094c3f4f
Milos Pruginic <milos_pruginic@yahoo.com>
2026-09-26 01:19:55 -0400

    Add: sort-colors - submission-2

---
+++ Data Structures & Algorithms/sort-colors/submission-2.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    sortColors(nums) {
+        let low = 0;
+        let mid = 0;
+        let high = nums.length - 1;
+
+        while(mid <= high) {
+            if(nums[mid] === 0) {
+                const temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] === 1) {
+                mid++;
+            } else {
+                const temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+
+        return nums;
+    }
+}
